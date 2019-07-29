eda98077cf0c973a26762271e5cd1f31a03e0f0a
Кунукролог <protap83@gmail.com>
2019-07-29 03:36:50 +0300

    Finish change Auth and Registation form

--- src/auth/Auth.js
+++ src/auth/Auth.js
@@ -7,37 +7,93 @@ export default class Auth extends Component {
         super(props);
         this.state = {
             isAuth: false,
-            password: '123'
+            isAuthForm: true,      // 
+            showPassword: 'password',
+            showPass: false,
+            login: '',
+            password1: '',
+            password2: '',
         };
-        this.loginHandler = this.loginHandler.bind(this);
+        // this.loginHandler = this.loginHandler.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
+        this.changeFormHandler = this.changeFormHandler.bind(this);
+        this.submitHandler = this.submitHandler.bind(this);
     }
 
-    loginHandler(e) {
-        // const login = () => e.target.getElementByID('login').value;
-        const login = (e) => e.target.getElementById('login').value;
-        console.log(login)
+    // loginHandler(e) {
+    //     console.log()
+    // }
+
+    changeFormHandler = (e) => {
+        // let is_auth = this.state.isAuthForm;
+        this.setState({
+            isAuthForm: !this.state.isAuthForm,
+        });
+        e.preventDefault();
     }
 
-    registerHandler = () => {
-
+    submitHandler(e) {
+        console.log('Login: ' + this.state.login);
+        console.log('Password 1: ' + this.state.password1);
+        console.log('Password 2: ' + this.state.password2);
+        e.preventDefault();
     }
 
-    authHandler = e => {
-        e.preventDefault()
-        console.log('Submited!')
+    changeHandler(e) {
+        
+        if (e.target.type === 'checkbox') {   // checking what value of element was changed
+                if (e.target.checked)       // if is it checkbox, then we show or hide pass
+                    this.setState({showPassword: 'text'})
+                else  
+                    this.setState({showPassword: 'password'})
+            } 
+            else
+                this.setState({[e.target.name]: e.target.value}); //if is it input fields, then we render tham with new values
+    
+        
     }
 
     render() {
+        let password2, 
+            titleFooterForm,
+            titleLinkFrom,
+            titleHeaderForm;   // draw & show 2-nd password field if change form type
+        if (!this.state.isAuthForm) {  //if is it registration form render 2-nd password field
+            password2 = 
+            <p>
+                <label htmlFor = "password2">Re-type Password:</label><br/>
+                <input 
+                className = "auth-input" 
+                name = "password2"
+                type = {this.state.showPassword}
+                value = {this.state.password}
+                onChange = {this.changeHandler} 
+                placeholder = "Re-Enter password"
+                />
+            </p>
+            titleHeaderForm = 'Registration';
+            titleLinkFrom = 'Login';
+            titleFooterForm = 'Already have account: ';
+        }   else {
+            titleHeaderForm = 'Log in';
+            titleLinkFrom = 'Registration';
+            titleFooterForm = 'Create an account: ';
+        }
+    
+
         return(
             <div className = "auth-form">
-                <form action="#" onSubmit = {this.loginHandler} >
-                    <h2 className = "auth-header-title">Log in</h2>
+                <form onSubmit = {this.submitHandler} >
+                    <h2 className = "auth-header-title">{titleHeaderForm}</h2>
                     <p>
                         <label htmlFor = "login">User name:</label><br/>
                         <input 
                             className = "auth-input" 
-                            id = "login" 
+                            id = "login"
+                            name = "login" 
                             type = "text" 
+                            value = {this.state.login}
+                            onChange = {this.changeHandler}
                             placeholder = "Enter your username" 
                             autoComplete = "off"
                         />
@@ -47,16 +103,24 @@ export default class Auth extends Component {
                         <input 
                             className = "auth-input" 
                             id = "password" 
-                            type = "password" 
+                            name = "password1"
+                            type = {this.state.showPassword}
+                            value = {this.state.password}
+                            onChange = {this.changeHandler} 
                             placeholder = "Enter your password"
                         />
                     </p>
+
+                    {password2}
+
                     <p>
                         <label>
                             <input 
                                 className = "checkbox" 
                                 type = "checkbox" 
-                                name = "showPassword"                        
+                                name = "showPassword"  
+                                checked = {this.state.hiddenPass}
+                                onChange = {this.changeHandler}                      
                             />
                             Show password
                         </label>
@@ -65,13 +129,14 @@ export default class Auth extends Component {
                         className = "submint" 
                         type = "submit" 
                         id = "submit"
+                        name = "submit"
                         onClick = {this.loginHandler}
                     >
                         Login
                     </button>
                     <p className = "auth-footer-title">
-                        Create an account 
-                        <a href = "localhost" onClick = {this.registerHandler}> Registration </a>
+                        {titleFooterForm} 
+                        <a href = "/" onClick = {this.changeFormHandler}> {titleLinkFrom} </a>
                     </p>
                 </form>
             </div>
