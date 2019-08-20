f2b93d185adf4a5709407efcc4d4cd3d3c3c3fd4
Кунукролог <protap83@gmail.com>
2019-08-20 08:24:20 +0300

    begin NavBar Notify

--- src/Components/UI/ProfileBar/index.js
+++ src/Components/UI/ProfileBar/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {ProfileBar, ArrowDropDownIcon } from './ProfileBar'
 import UserPic from '../UserPic'
+import NewMessages from '.'
 import data from '../../../Store/data.json'
 
 export default class index extends Component {
@@ -13,6 +14,7 @@ export default class index extends Component {
         const userData = data.results[3];
         return (
             <ProfileBar>
+                <NewMessages />
                 <p>{userData.name.first +' '+ userData.name.last}</p>
                 <UserPic url={userData.picture.large} size="50px" />
                 <ArrowDropDownIcon  size="32"/>
